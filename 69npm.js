4956cb86679191279fded7a60a82540d8c4f8c28
ramnareshji <119052834+ramnareshji@users.noreply.github.com>
2023-02-06 00:16:21 +0530

    Add files via upload

---
+++ 69npm.js
@@ -0,0 +1,3 @@
+console.log("This is npm practise ");
+console.log("this is running after installing, npm i nodemon --global");
+console.log("In this video i have worked on TERMINAL by typing code , run first code , npm init , then file will be ready to download other packages")
